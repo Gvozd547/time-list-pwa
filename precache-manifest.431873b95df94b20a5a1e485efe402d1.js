33b7229e8ed9f7b09f962cbaead349a4221ed1f1
vv.barcic <vv.barcic@tensor.ru>
2020-08-13 11:17:39 +0700

    Updates

---
+++ precache-manifest.431873b95df94b20a5a1e485efe402d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "607f60cb23330f5670c3c76883e3737e",
+    "url": "/time-list-pwa/index.html"
+  },
+  {
+    "revision": "411298f22818a2a64c6f",
+    "url": "/time-list-pwa/static/css/main.4f4887bb.chunk.css"
+  },
+  {
+    "revision": "2054b5603874bd7e5818",
+    "url": "/time-list-pwa/static/js/2.ecf815fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/time-list-pwa/static/js/2.ecf815fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "411298f22818a2a64c6f",
+    "url": "/time-list-pwa/static/js/main.de2840a3.chunk.js"
+  },
+  {
+    "revision": "1427dca1453f24d344a9",
+    "url": "/time-list-pwa/static/js/runtime-main.2fb09569.js"
+  }
+]);
---
+++ sw.js
@@ -0,0 +1,61 @@
+var doCache = true;
+
+var CACHE_NAME = 'my-time-list-cache';
+
+self.addEventListener("activate", event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys()
+      .then(keyList =>
+        Promise.all(keyList.map(key => {
+          if (!cacheWhitelist.includes(key)) {
+            console.log('Deleting cache: ' + key)
+            return caches.delete(key);
+          }
+        }))
+      )
+  );
+});
+
+self.addEventListener('install', function(event) {
+  if (doCache) {
+    event.waitUntil(
+      caches.open(CACHE_NAME)
+        .then(function(cache) {
+          fetch("/asset-manifest.json")
+            .then(response => {
+              return response.json()
+            })
+            .then(assets => {
+              const arrAssets = Object.values(assets.files);
+              const urlsToCache = [
+                "/",
+                "/db.json",
+                "/favicon.ico",
+                ...arrAssets
+              ]
+              cache.addAll(urlsToCache)
+              console.log('cached');
+            })
+        })
+    );
+  }
+});
+self.addEventListener('fetch', function(event) {
+    if (doCache) {
+      event.respondWith(
+          caches.match(event.request).then(function(response) {
+              return response || fetch(event.request);
+          })
+      );
+    }
+});
+self.addEventListener('push', function(event) {
+  var payload = event.data ? event.data.text() : 'Alohomora';
+  
+  event.waitUntil(
+    self.registration.showNotification('My first spell', {
+      body: payload,
+    })
+  );
+});
